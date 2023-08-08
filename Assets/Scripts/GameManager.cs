a3c4147381783a4870a9c18a3051699db22408a6
Michael Durst <Michael.Durst@yahoo.com>
2023-08-08 14:52:38 -0700

    Adding GameManager
    May10,2023 Class Work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Represent the GameManager. 
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Attempted to create a second instance of the Game Manager.");
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
